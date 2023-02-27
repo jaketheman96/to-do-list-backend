6ef9015c79b9cc92268c3c65c9a86af623c3d7fc
Jake Chien <jake_chien@outlook.com>
2023-02-27 18:52:06 -0300

    feat: api configured ready for use

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import ErrorHandler from './middlewares/ErrorHandler';
+
+const PORT = process.env.PORT || 3001
+
+const app = express();
+
+app.use(express.json());
+app.get('/', () => console.log('Get funcionando'))
+app.use(ErrorHandler.handle)
+
+app.listen(PORT, (): void => {
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
+
+export default app;
