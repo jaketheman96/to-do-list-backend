feeb1e9bd40093bee6178642e8a222ddfda13ede
Jake Chien <jake_chien@outlook.com>
2023-03-01 20:18:16 -0300

    feat: add get task by id as id validator

--- backend/src/api/services/TaskService.ts
+++ backend/src/api/services/TaskService.ts
@@ -1,5 +1,6 @@
 import Tasks from "../../database/models/Task";
 import Itask from "../../interfaces/Itask";
+import statusCodes from "../../utils/statusCode";
 
 class TaskService {
   async getAllTasks(): Promise<Itask[]> {
@@ -7,21 +8,38 @@ class TaskService {
     return allTasks;
   }
 
+  async getTaskById(id: string): Promise<number | Itask> {
+    const task = await Tasks.findByPk(id)
+    if (!task) return statusCodes.NOT_FOUND;
+    return task;
+  }
+
   async postTask(taskInfos: Itask): Promise<void> {
     await Tasks.create(taskInfos);
     return;
   }
 
-  async editTask(id: string, taskInfos: Itask): Promise<void> {
-    await Tasks.update(taskInfos, { where: { id } })
+  async editTask(id: string, taskInfos: Itask): Promise<number | void> {
+    const idValidation = await this.getTaskById(id);
+    if (idValidation === 404) return statusCodes.NOT_FOUND;
+    await Tasks.update(taskInfos, { where: { id } });
     return;
   }
 
-  async doneStatus(id: string): Promise<void> {
+  async doneStatus(id: string): Promise<number | void> {
+    const idValidation = await this.getTaskById(id);
+    if (idValidation === 404) return statusCodes.NOT_FOUND;
     await Tasks.update(
       { done: true },
       { where: { id } }
     );
+    return;
+  }
+
+  async deleteTask(id: string): Promise<number | void> {
+    const idValidation = await this.getTaskById(id);
+    if (idValidation === 404) return statusCodes.NOT_FOUND;
+    await Tasks.destroy({ where: { id } });
     return;
   }
 }
